5dd89ffc5e910cfe56eee9b21f7856408c60227c
migo1 <ertmigot@gmail.com>
2023-07-13 14:21:23 +0300

    add-category-route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import Books from './components/Books/Books';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </div>
     </div>
---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,9 @@
+function Categories() {
+  return (
+    <div>
+      <h1>Categories Comming Soon ....</h1>
+    </div>
+  );
+}
+
+export default Categories;
